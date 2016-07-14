205ac18d665ead4f3cd23a5b3fdf4095f5d29b3c
Sergey Badulin <sbadulin@gmail.com>
2016-07-14 21:22:58 +0300

    email set for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,23 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "learn-rails_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # email enabled in production
+  config.action_mailer.smtp_settings = {
+    address: "smtp.postmarkapp.com",
+    port: 25,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    user_name: Rails.application.secrets.email_provider_api,
+    password: Rails.application.secrets.email_provider_api,
+    enable_starttls_auto: true
+  }
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
